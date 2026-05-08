15c48d3a49a16adff3b45c3367b352de71c31420
tmironovich <taniamironovich@vk.com>
2026-05-08 11:41:47 +0500

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let num = n;
+  if (num < 0) {
+    num = Math.abs(num);
+  }
+  let res = 0;
+  while (num > 0) {
+    const lastNum = num % 10;
+    res = res * 10 + lastNum;
+    num = Math.floor(num / 10);
+  }
+  return res;
 };
